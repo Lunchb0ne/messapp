60e5cb900706460e974ed54aeed702415007781e
Abhishek Aryan <abhishek.rbg@gmail.com>
2018-08-08 08:46:55 +0530

    Code Cleanup and ideas roll in
    Cleaning up the code and shift the css to the sass folder
    
    setup todo things

---
+++ js/indexabh.js
@@ -0,0 +1,44 @@
+console.clear();
+
+var el = {};
+
+var seq;
+
+$('.placeholder').on('click', function (ev) {
+    $('.placeholder').css('opacity', '0');
+    $('.list__ul').toggle();
+});
+
+$('.list__ul a').on('click', function (ev) {
+    //ev.preventDefault();
+    var index = $(this).parent().index();
+    
+    $('.placeholder').text( $(this).text() ).css('opacity', '1');
+    
+    console.log($('.list__ul').find('li').eq(index).html());
+    
+    seq = $('.list__ul').find('li').eq(index).html();
+    $('.list__ul').find('li').eq(index).prependTo('.list__ul');
+    $('.list__ul').toggle();
+});
+
+
+$('select').on('change', function (e) {
+    // Set text on placeholder hidden element
+    $('.placeholder').text(this.value);
+    
+    // Animate select width as placeholder
+    $(this).animate({width: $('.placeholder').width() + 'px' });
+});
+
+/*
+
+Project MAP:
+
+1- Hide the selection part after selection of day
+2- Go pass the day which we got to the menuGetter
+3- animate the cards via jQuery
+4- give option to select another date => proceed to reload the page
+
+
+*/
